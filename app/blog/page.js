f0cfc4786b9cc2468ca3207be93aa0886aee55b1
aminul157246 <aminul.islam.157246@gmail.com>
2024-04-05 08:41:50 +0600

    added blog page

--- app/blog/page.js
+++ app/blog/page.js
@@ -3,7 +3,8 @@ import React from 'react';
 const BlogPage = () => {
     return (
         <div>
-            <h2>This is Blog page</h2>
+            <h3>I am BlogPage
+                BlogPage</h3>
         </div>
     );
 };
